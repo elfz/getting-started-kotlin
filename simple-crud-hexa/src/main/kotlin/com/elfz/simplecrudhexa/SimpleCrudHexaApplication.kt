6dfabc9991ce4c88a10268112395b6976a8ab791
Douglas Henrique dos Santos <dohenrique@uolinc.com>
2021-04-01 14:48:31 -0300

    Added configuration and readme

--- simple-crud-hexa/src/main/kotlin/com/elfz/simplecrudhexa/SimpleCrudHexaApplication.kt
+++ simple-crud-hexa/src/main/kotlin/com/elfz/simplecrudhexa/SimpleCrudHexaApplication.kt
@@ -1,9 +1,12 @@
 package com.elfz.simplecrudhexa
 
+import com.elfz.simplecrudhexa.properties.S3Properties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableConfigurationProperties(value = [S3Properties::class])
 class SimpleCrudHexaApplication
 
 fun main(args: Array<String>) {
