0ee135d46a856bbbe225cf24a1c269015e669f10
Douglas Henrique dos Santos <dohenrique@uolinc.com>
2021-04-01 15:10:38 -0300

    Fix null problem

--- simple-crud-hexa/src/main/kotlin/com/elfz/simplecrudhexa/adapter/out/persistence/FindNoteFromFilePersistenceAdapter.kt
+++ simple-crud-hexa/src/main/kotlin/com/elfz/simplecrudhexa/adapter/out/persistence/FindNoteFromFilePersistenceAdapter.kt
@@ -12,23 +12,25 @@ class FindNoteFromFilePersistenceAdapter(
     private val s3Repository: S3Repository
 ) : FindNoteFromFilePersistencePort {
 
-    override fun findFrom(source: String, fileName: String): List<NoteDomain> {
-        val findS3Object = s3Repository.findS3Object(source, fileName)
-
-        val inputStream = findS3Object.objectContent
-        val reader = BufferedReader(InputStreamReader(inputStream))
-        reader.readLine() // ignore first line
-
-        return reader.lines()
-            .map {
-                val list = it.split(',')
-                val id = list[0].toLong()
-                val title = list[1]
-                val description = list[2]
-                NoteDomain(title, description, id)
-            }.toList()
-    }
-
+    override fun findFrom(source: String, fileName: String) =
+        s3Repository
+            .findS3Object(source, fileName)
+            .objectContent
+                ?.let { InputStreamReader(it) }
+                ?.let { BufferedReader(it) }
+                ?.apply { readLine() } // ignore first line
+                ?.let { it ->
+                  it.lines()
+                      ?.map {
+                        val list = it.split(',')
+                        val id = list[0].toLong()
+                        val title = list[1]
+                        val description = list[2]
+                        NoteDomain(title, description, id)
+                      }
+                      ?.toList()
+                }
+                ?: emptyList()
 }
 
 
